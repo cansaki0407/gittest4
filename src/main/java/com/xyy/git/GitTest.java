e72566dc07b0018ac124958c08aa8e27a7dde46c
pipi <510702003@qq.com>
2022-04-10 20:10:10 +0800

    first commit

---
+++ src/main/java/com/xyy/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.xyy.git;
+
+/**
+ * @author 皮皮瑶
+ * @proname
+ * @data 2022/4/10- 20:06
+ */
+public class GitTest {
+	public static void main(String[] args) {
+		System.out.println("git is so easy");
+	}
+}
