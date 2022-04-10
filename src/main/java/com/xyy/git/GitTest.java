cd47b0d6e101904f0b73562fb4fd1e11d2a85702
pipi <510702003@qq.com>
2022-04-10 20:12:43 +0800

    third commit

--- src/main/java/com/xyy/git/GitTest.java
+++ src/main/java/com/xyy/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
 	public static void main(String[] args) {
 		System.out.println("git is so easy");
 		System.out.println("github is so noob");
+		System.out.println("gitee is so good");
 	}
 }
